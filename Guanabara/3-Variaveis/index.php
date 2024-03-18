4e2c5f03386567dd7e6cf43ea179fcb3c71ffe42
Lugh-o <lughfalcao@gmail.com>
2024-03-18 17:05:59 -0300

    18/03 Guanabara

---
+++ Guanabara/3-Variaveis/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $nomeCompletoCliente = "Lucas Carneiro Falcão"; 
+    //variáveis, métodos e funções -> camelCase 
+    const PAIS_NATAL = "Brasil"; 
+    //constantes -> SNAKE_CASE
+    
+    echo "Olá, $nomeCompletoCliente! Você mora no " . PAIS_NATAL;
+    ?>
+</body>
+</html>
